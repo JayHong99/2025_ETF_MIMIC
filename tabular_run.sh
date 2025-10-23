a7f71ed434635ccc96e7b57fba7cb23695e3e4e0
jayhong99 <jayhong7200@gmail.com>
2025-10-23 08:15:00 +0000

    updated

--- tabular_run.sh
+++ tabular_run.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 source ../robust_venv/bin/activate
 
-cfg_path="configs/tabular_mortality_90days_BS_512_hidden_128.yaml"
-# cfg_path="configs/tabular_readmission_15days_BS_512_hidden_128.yaml"
-device=0
-
-# limit cpu usage ~20%
-
-# for cfg_path in "configs/tabular_mortality_90days_BS_512_hidden_128.yaml" "configs/tabular_readmission_15days_BS_512_hidden_128.yaml"; do
-#     for seed in 2026 2027 2028; do
-#         python linear_run.py --cfg_path "$cfg_path" --device $device --seed $seed &
-#     done
-#     wait
-# done
-
-# sample
-cfg_path="configs/tabular_mortality_90days_BS_512_hidden_128.yaml"
+cfg_path1="configs/tabular_mortality_90days_BS_512_hidden_128.yaml"
+cfg_path1="configs/tabular_readmission_15days_BS_512_hidden_128.yaml"
 device=1
-seed=2026
-python linear_run.py --cfg_path "$cfg_path" --device $device --seed $seed --do_train False
+
+
+for cfg_path in "$cfg_path1" "$cfg_path2"; do
+    for seed in 2026 2027 2028; do
+        python linear_run.py --cfg_path "$cfg_path" --device $device --seed $seed &
+    done
+    wait
+done
